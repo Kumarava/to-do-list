2351d465232f8940156b7106758316c7b577746d
Kumarava <kozhobaev@gmail.com>
2020-06-25 21:17:04 +0200

    initial commit

---
+++ src/Main/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Todos } from "../Todos";
+export const Main = () => {
+  return <main className="App-main">MAIN</main>;
+};
+const TodoMain = () => {
+  return (
+    <div>
+      <Todos />
+    </div>
+  );
+};
+
+export default TodoMain;
---
+++ src/Todos/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+export const Todos = () => {
+  return (
+    <div>
+      <ul class="to-do-list"></ul>
+      <li>Banana</li>
+      <li>Apple</li>
+      <li>Milk</li>
+      <hr />
+    </div>
+  );
+};
