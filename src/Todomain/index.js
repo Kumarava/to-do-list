785776e05176791a162229125fb8b78e94fe4775
Kumar <kozhobaev@gmail.com>
2020-08-09 22:49:05 +0200

    adding database

--- src/Todomain/index.js
+++ src/Todomain/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Todos } from "../Todos";
 import { NewToDoForm } from "../NewToDoForm";
 import { Filtering } from "../Filtering";
@@ -8,33 +8,54 @@ export const Todomain = () => {
   const [showAll, setShowAll] = React.useState(false);
   const [todos, setTodos] = React.useState([]);
   let { userId } = useParams();
-  React.useEffect(() => {
-    const fetchTodos = async () => {
-      const response = await fetch(
-        `https://merey-todo-list.herokuapp.com/api/users/${userId}/todos`
-      );
 
-      const todosData = await response.json();
-      setTodos(todosData);
-    };
-    fetchTodos();
+  const fetchTodos = useCallback(async () => {
+    const response = await fetch(
+      `https://merey-todo-list.herokuapp.com/api/users/${userId}/todos`
+    );
+    const todosData = await response.json();
+    setTodos(todosData);
   }, [userId]);
 
-  const handleAddTodo = (todoTitle) => {
-    const newTodo = {
-      id: todos.length,
-      title: todoTitle,
-      done: false,
-    };
-    setTodos([...todos, newTodo]);
+  React.useEffect(() => {
+    fetchTodos();
+  }, [fetchTodos]);
+
+  const handleAddTodo = async (todoTitle) => {
+    const response = await fetch(
+      `https://merey-todo-list.herokuapp.com/api/todos/${userId}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title: todoTitle }),
+      }
+    );
+
+    const taskData = await response.json();
+    if (!taskData.taskId) alert("Did not added to database");
+    else fetchTodos();
   };
 
-  const handleChangeTodo = (id) => {
-    const newTodos = [...todos];
-    const todo = newTodos.find((duetodo) => duetodo.id === id);
-    todo.done = !todo.done;
-    setTodos(newTodos);
+  const handleChangeTodo = async (id) => {
+    const item = todos.find((todo) => id === todo._id);
+    const response = await fetch(
+      `https://merey-todo-list.herokuapp.com/api/todos/${id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ done: !item.done }),
+      }
+    );
+
+    const taskData = await response.json();
+    if (!taskData.taskId) alert("Property wasn't changed on database");
+    else fetchTodos();
   };
+
   if (!userId) return <h1>Please select User</h1>;
   return (
     <Paper>
